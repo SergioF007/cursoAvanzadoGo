939fd39c60d12532e5eaedd60f8d23f434a19fc1
Sergio Fernández <sefernandez0711@gmail.com>
2023-08-16 08:36:47 -0500

    Programa: Escanear puertos de un servidor, sin y con  concurrencia

---
+++ net/port.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"sync"
+)
+
+var site = flag.String("site", "scanme.nmap.org", "urL to scan")
+
+func main() {
+	flag.Parse() // tomamos los parametros que se implmentan con flag y los vamos a dejar disponibles dentro de la variable site
+	var wg sync.WaitGroup
+
+	for port := 1; port < 65535; port++ {
+		wg.Add(1)
+		go func(port int) {
+			defer wg.Done()
+			conexion, err := net.Dial("tcp", fmt.Sprintf("%s: %d", *site, port))
+			if err != nil {
+				return
+			}
+			conexion.Close()
+			fmt.Printf("Port %d is open\n", port)
+
+		}(port)
+	}
+	wg.Wait()
+}
